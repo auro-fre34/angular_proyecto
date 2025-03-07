1819cbc15eddce4d1cc341959f5c863cccd4278d
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-03-07 14:20:12 -0600

    product-component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
+    standalone: true,
     selector: 'app-produc',
     template: './product.component.html',
     styleUrls: ['./product.component.css']
