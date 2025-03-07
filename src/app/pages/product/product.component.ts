0846d3a41c3215a8f4f6541fde1bba9c1d00262c
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-03-07 14:08:14 -0600

    subir proyecto

---
+++ src/app/pages/product/product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-produc',
+    template: './product.component.html',
+    styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+}
