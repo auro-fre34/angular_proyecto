b207630b2053fe9197f8cf65c46dad1f9a28ab89
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-03-07 17:18:23 -0600

    Product component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { LoaderComponent } from "../../components/loader/loader.component";
 
 @Component({
     standalone: true,
     selector: 'app-product',
     templateUrl: './product.component.html',
-    styleUrls: ['./product.component.scss']
+    styleUrls: ['./product.component.scss'],
+    imports: [
+        CommonModule,
+        LoaderComponent
+    ]
 })
 export class ProductComponent {
 
